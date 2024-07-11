1931ad53db735ec2b3b79c88889a0abd57dd2e81
Sugam <129360173+SUGAM2001@users.noreply.github.com>
2024-07-11 23:36:38 +0530

    Code

---
+++ PyCode/Convert_DNA_RNA.py
@@ -0,0 +1,17 @@
+# import library
+from Bio.Seq import Seq
+
+def dna_to_rna(dna_sequence):
+  #seq object
+  dna_seq = Seq(dna_sequence)
+
+ # Use the transcribe method to convert dna to rna
+  rna_seq = dna_seq.transcribe()
+  return ran_seq
+
+dna_seq = "ATGCATCCGATCTGACTGCATC"
+rna_sequence = dna_to_rna(dna_sequence)
+print(f"DNA sequence: {dna_seq}")
+print(f"RNA sequence: {rna_seq}")
+
+
