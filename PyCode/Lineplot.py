14536bbaf0f3b9b88f10fcdcf7d7305bf8bc9552
Sugam <129360173+SUGAM2001@users.noreply.github.com>
2024-07-22 22:09:57 +0530

    Code

---
+++ PyCode/Lineplot.py
@@ -0,0 +1,20 @@
+# import library
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+file_path = "C:/Users/sugam patel/Downloads/filtered_gene_expression_counts.csv" 
+
+data = pd.read_csv(file_path)
+data.head()
+
+# Create Line Plot
+plt.figure(figsize=(10, 6))
+for sample in data.columns:
+    plt.plot(data.index, data[sample], label=sample)
+plt.title('Line Plot of Gene Expression Levels')
+plt.xlabel('Sample Index')
+plt.ylabel('Expression Level')
+plt.legend()
+plt.show()
+
